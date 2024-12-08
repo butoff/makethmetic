92cf586d6eb090ea1bb5db5b543cd6fad1a6e90d
Roman Butov <butoff@yandex-team.ru>
2024-12-08 20:13:37 +0300

    operators and calc function

--- makefile
+++ makefile
@@ -1,28 +1,31 @@
-# This makefile is intended to test functions in makethmetic.mk
+# This makefile is intended to exercise functions in makethmetic.mk
+# For testing, just call make(1) within the directory containing this file
 
 include makethmetic.mk
 
-# TODO not working yet
-define assert
-$(info ASSERT '$(value $(1))' == '$(2)' $(shell echo "\033[32mOK\033[0m"))
-endef
+_succ := 0
+_fail := 0
+_all := 0
 
-$(call test,2,2)
+assert = $(info assert $1 = $2:\
+  $(if $(filter $2,$(call calc,$1)),\
+  $(shell echo "\033[32mOK\033[0m")\
+    $(eval _succ = $(call calc,1 + $(_succ))),\
+  $(shell echo "\033[31mFAIL: expected $2, actual $(call calc,$1)\033[0m")\
+    $(eval _fail = $(call calc,1 + $(_fail)))))
+    $(eval _all = $(call calc,1 + $(_all)))
 
-test:
-	# 1981 + 42 = $(call add,1981,42)
-	# 2024 - 1981 = $(call sub,2024,1981)
-	# 6 * 8 = $(call mul,6,8)
-	# 48 / 8 = $(call div,48,8)
-	# 3 / 2 = $(call div,3,2)
-	# 3 % 2 = $(call rem,3,2)
-	# 9 / 3 = $(call div,9,3)
-	# 9 % 3 = $(call rem,9,3)
-	# 9 / 2 = $(call div,9,2)
-	# 9 % 2 = $(call rem,9,2)
-	# 7 / 3 = $(call div,7,3)
-	# 1 / 2 = $(call div,1,2)
-	# 1 / 1 = $(call div,1,1)
-	# 2 / 2 = $(call div,2,2)
-	# 2 % 2 = $(call rem,2,2)
-	# 2 / 4 = $(call div,2,4)
+$(call assert,1 + 0,1)
+$(call assert,2 + 3,5)
+$(call assert,3 + 2,5)
+$(call assert,3 - 2,1)
+$(call assert,3 * 2,6)
+$(call assert,13 / 4,3)
+
+_all := $(call calc,$(_succ) + $(_fail))
+
+.PHONY: summary
+
+summary:
+	@echo --------------------------------------------------------
+	@echo $(_all) tests executed, $(_succ) passed, $(_fail) failed
--- makethmetic.mk
+++ makethmetic.mk
@@ -1,39 +1,40 @@
-# exponentially grows a list of 'x' (x-list) given in $(2) untill its length
-# exceeds an length given in $(1)
-# e.g. $(call _grow,5,x x) produces the string within quotes: 'x x x x x x x x'
-_grow = $(if $(word $(1),$(2)),$(2),$(call _grow,$(1),$(subst x,x x,$(2))))
+# Exponentially grows a list of '|' (stick-list) given in $(2) untill its length
+# exceeds a length given in $(1)
+# E.g. $(call _grow,5,| |) produces the string within quotes: '| | | | | | | |'
+_grow = $(if $(word $(1),$(2)),$(2),$(call _grow,$(1),$(subst |,| |,$(2))))
 
-# converts a decimal number N given in $(1) to its "scattered" representation
-# as a list of N 'x' (x-list)
-# e.g. $(call _scatter,5) gives 'x x x x x'
+# Converts a decimal number N given in $(1) to its "scattered" representation
+# as a list of N '|' (stick-list)
+# e.g. $(call _scatter,5) gives '| | | | |'
 # The opposite operation can be done by the "words" built-in function
-_scatter = $(wordlist 1,$(1),$(call _grow,$(1),x x))
+_scatter = $(if $(filter 0,$(1)),,$(wordlist 1,$(1),$(call _grow,$(1),| |)))
 
-# tests whether the x-list in $(1) is greater or equal to the x-list in $(2)
+# Tests whether the stick-list in $(1) is greater or equal to the stick-list in $(2)
 _greater_or_equal = $(word $(words $(2)),$(1))
 
-# adds (i.e. concatenates) two x-lists
+# Adds (i.e. concatenates) two stick-lists
 _add = $(1) $(2)
 
-# multiplicates two x-lists
-_mul = $(subst x,$(1),$(2))
+# Multiplicates two stick-lists
+_mul = $(subst |,$(1),$(2))
 
-# subtracts a x-list given in $(2) from the x-list given in $(1)
-_sub = $(filter-out xx,$(join $(1),$(2)))
+# Subtracts a stick-list given in $(2) from the stick-list given in $(1)
+_sub = $(filter-out ||,$(join $(1),$(2)))
 
-# divides a x-list given in $(1) by the x-list given in $(2)
-_div = $(if $(call _greater_or_equal,$(1),$(2)),x $(call _div,$(call _sub,$(1),$(2)),$(2)),)
+# Divides a stick-list given in $(1) by the stick-list given in $(2)
+_div = $(if $(call _greater_or_equal,$(1),$(2)),| $(call _div,$(call _sub,$(1),$(2)),$(2)),)
 
-# adds two decimal values
-add = $(words $(call _add,$(call _scatter,$(1)),$(call _scatter,$(2))))
+# Adds two decimal values
++ = $(words $(call _add,$(call _scatter,$(1)),$(call _scatter,$(2))))
 
-# multiplicates two decimal values
-mul = $(words $(call _mul,$(call _scatter,$(1)),$(call _scatter,$(2))))
+# Multiplicates two decimal values
+* = $(words $(call _mul,$(call _scatter,$(1)),$(call _scatter,$(2))))
 
-# subtracts decimal value given in $(2) from the decimal $(1)
-# !! Negative values not supported yet.
-# !! A caller is responsible for holding $(1) > $(2)
-sub = $(words $(call _sub,$(call _scatter,$(1)),$(call _scatter,$(2))))
+# Subtracts decimal value given in $(2) from the decimal $(1)
+- = $(words $(call _sub,$(call _scatter,$(1)),$(call _scatter,$(2))))
 
-# divids decimal value given in $(1) by the decimal $(2)
-div = $(words $(call _div,$(call _scatter,$(1)),$(call _scatter,$(2))))
+# Divids decimal value given in $(1) by the decimal $(2)
+/ = $(words $(call _div,$(call _scatter,$(1)),$(call _scatter,$(2))))
+
+# Calculates expression in $(1), which has a form "| (operator) y"
+calc = $(call $(word 2,$(1)),$(word 1,$(1)),$(word 3,$(1)))
